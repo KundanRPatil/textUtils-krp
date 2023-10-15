0b009ba1e4e73b49e75ddd91dc0d9a1f2bf6a22c
Kundan Patil <kundanpatil932577@gmail.com>
2023-10-15 14:08:09 +0530

    Updates

---
+++ precache-manifest.ac370e71d3592ad2ef98302564f87608.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d5e5b21b2811cd4a67a8f5fe213a26",
+    "url": "/KundanRPatil/textUtils-krp/index.html"
+  },
+  {
+    "revision": "bd25a18d100ae4dfc74d",
+    "url": "/KundanRPatil/textUtils-krp/static/js/2.421d52ea.chunk.js"
+  },
+  {
+    "revision": "5854eb9be6dd082bd048",
+    "url": "/KundanRPatil/textUtils-krp/static/js/3.524ce66f.chunk.js"
+  },
+  {
+    "revision": "c0fa0b706d19c586e6ed",
+    "url": "/KundanRPatil/textUtils-krp/static/js/main.9e1dc664.chunk.js"
+  },
+  {
+    "revision": "a4b40ef271916eadf618",
+    "url": "/KundanRPatil/textUtils-krp/static/js/runtime~main.2f9ad228.js"
+  }
+]);
