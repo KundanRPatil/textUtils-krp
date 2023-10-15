fe95bab182105bf726feebd524963ee8131ace89
Kundan Patil <kundanpatil932577@gmail.com>
2023-10-15 14:37:56 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/KundanRPatil/textUtils-krp/precache-manifest.b792086b528923ba867acd901614dc6e.js"
+  "/textUtils-krp/precache-manifest.95eab8b3957ca8f6479aa1b1c0e48790.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/KundanRPatil/textUtils-krp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/textUtils-krp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
